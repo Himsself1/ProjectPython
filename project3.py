becb3dd23d54ffc43b5e9cf515e2397077cc9ae2
Himsself1 <spapadadonakis@gmail.com>
2018-03-13 16:27:40 +0200

    COMMENTS COMMENTS AND GUESS WHAT COMMENTS

--- project3.py
+++ project3.py
@@ -73,10 +73,16 @@ def read_vcf_file(file_name, start=0, end=None):
 		raise Exception("Invalid File Extension. File needs to end with .vcf or .vcf.gz")
 
 def vcf_info(file_name, start = 0, end = None) :
+        '''
+        Prints how many samples and SNPs are in your .vcf file
+        '''
 	df=read_vcf_file(file_name, start, end)
 	print ("File has", df.shape[1]-9,"samples","\n","File has",df.shape[0],"SNPs" )
 
 def sample_info(sample_file):
+        '''
+        Prints how many populations and super-populations (geographic regions) are in the sample information file
+        '''
 	df=pd.read_csv(sample_file, sep="\t", header = 0)
 	print ("File has", len(df.super_pop.unique()), "Areas.")
 	for i in range(0,len(df.super_pop.unique())) :
@@ -89,12 +95,15 @@ def sample_info(sample_file):
 		for k in range(0,len(q['pop'].unique())) :
 			print (q['pop'].unique()[k], q[(q["pop"]==q['pop'].unique()[k])].shape[0],"samples")
 
-def validate_sample(file_name, sample_file ) :
+def validate_sample(file_name, sample_file ):
+        '''
+        Confirms if the sample infomation file and the .vcf file have the same individuals
+        '''
 	our_samples=np.array(read_vcf_file(file_name).columns.values[9:])
 	id_array=np.array(pd.read_csv(sample_file, sep="\t", header = 0)["sample"].tolist())
 	if np.array_equiv(np.sort(our_samples),np.sort(id_array)):
 		print ("Everything is OK!")
-	else :
+	else:
 		b = np.setdiff1d(id_array,our_samples)
 		q = np.setdiff1d(our_samples,id_array)
 		if len(b) != 0:
@@ -104,7 +113,8 @@ def validate_sample(file_name, sample_file ) :
 
 def convert_genotype_to_number(z):
 	'''
-	Converts a VCF genotype to numerical (zero one or two mutations per sample per SNP position) 
+	Converts a VCF genotype to numerical (zero one or two mutations per sample per SNP position)
+        <z> must be a numpy table or pandas table of genotypes 
 	'''
 	z = np.array(z)
 	z[ z == '0|0' ] = 0
@@ -113,15 +123,24 @@ def convert_genotype_to_number(z):
 	z[ z == '1|1' ] = 2
 	return z
 
-def convert_to_jaccard (z) :
+def convert_to_jaccard(z):
+	'''
+	Converts a VCF genotype to jaccard (zero or one mutations per sample per genotype position)
+        <z> must be a numpy table or pandas table of genotypes 
+	'''
+	z = np.array(z)
 	z[ z == '0|0' ] = 0
 	z[ z == '1|0' ] = 1
 	z[ z == '0|1' ] = 1
 	z[ z == '1|1' ] = 1
 	return z
 
-def jaccard_index (table) :
-	jac_table=convert_to_jaccard(table)
+def jaccard_index (table):
+        '''
+        calculates the jaccard matrix accordring to hamming distance
+        <table> must be a numpy table
+        '''
+	jac_table = convert_to_jaccard(table)
 	return 1 - pairwise_distances(table.T, metric = "hamming")
 
 def find_non_zero_AFs( dict_of_pop_freqs, min_af = 0.0 ):
@@ -160,6 +179,8 @@ def calculate_frequencies( data, individuals ):
 def population_columns( sample_matrix, pop_name ):
 	'''
 	Returns the names of all individuals of population <pop_name>
+        <sample_matrix> must be a pandas matrix of the sample_information file
+        <pop_name> is ONE POPULATION as string
 	'''
 	if sample_matrix['pop'].str.contains(pop_name).sum() == 0:
 		raise Exception ('Error, Wrong population name: {}'.format(pop_name))
@@ -191,6 +212,15 @@ def simulation_dependent( data, sample_matrix, pop_names, sample_sizes, total_sn
 	return finale
 
 def simulation_independent( data, sample_matrix, pop_names, sample_sizes, independent, min_af = 0 ):
+        '''
+	Simulates independent genotypes based on genotype frequencies of all populations in <pop_names>
+	<data> is the pandas DataFrame containing genotypes
+	<sample_matrix> is a pandas DataFrame containing information from the sample information file
+	<pop_names> is a python list with all the names of the populations provided by the command line
+	<sample_sizes> is a python dictionary with keys = pop_names and values = how many individuals are to be simulated for each population
+	<min_af> is the lnowest acceptable frequency of a SNP
+        <independent> is the number of SNPs to be simulated. It is provided by command line
+	'''
 	pop_vecs={}
 	for i in pop_names:
 		noumera = population_columns( sample_matrix, i )
@@ -207,19 +237,21 @@ def simulation_independent( data, sample_matrix, pop_names, sample_sizes, indepe
 
 def read_my_labels( my_vector ):
 	'''
-	Takes a pd.Series with label names and returns a dicttionary with 
-	keys = population labels and values = the indexes of individuals accroding to labeling
-	'''
+	Takes a pd.Series with label names and returns a dictionary with keys = population labels and values = the indexes of individuals accroding to labeling
+	<my_vector> is a pd.Series containing the names of the samples in following format: "POPNAME_INDEX"
+        '''
 	kefali = pd.DataFrame(my_vector.str.split("_").tolist(), columns=["population", "index"])
 	kefali["index"] = pd.to_numeric(kefali['index'])
-	aa = { i:[] for i in kefali['population'].unique() }
+        aa = { i:[] for i in kefali['population'].unique() }
 	[ aa[kefali.as_matrix()[i,0]].append(i) for i in range(0,kefali.shape[0]) ]
 	return aa
 
-def my_pca(np_table, jaccard=args.jaccard) :
+def my_pca(np_table, jaccard = None) :
 	'''
 	Principal component analysis to simulated data tables
-	'''
+	<np_table> is a numpy table
+        if <jaccard> = True, it performs PCA according to jaccard index
+        '''
 	if jaccard== None :
 		genotypes_array=convert_genotype_to_number(np_table)
 		pca = PCA(n_components=2)
@@ -232,21 +264,25 @@ def my_pca(np_table, jaccard=args.jaccard) :
 		genotypes_PCA = pca.transform(jaccard_index(np_table))
 	return genotypes_PCA
 
-def pca_plot(genotypes_PCA, header) :
+def pca_plot( genotypes_PCA, header, plot_filename ):
 	'''
-	Saves two-dimensional data produced b PCA
-	'''	
+	Plots two-dimensional data produced by PCA
+	<genotypes_PCA> is the output of the "my_pca" function
+        <headers> is a pd.Series containing the names of the samples in following format: "POPNAME_INDEX"
+        '''	
 	aa = read_my_labels(header)
 	left = 0
 	for i in aa.values:
 		plt.plot(genotypes_PCA[left:left+i[0],0], genotypes_PCA[left:left+i[0],1], '.', label=i)
 		left = left + i[0]
-	plt.savefig(args.PCA_plot)
+	plt.savefig(plot_filename)
 
 def find_majority( mia_lista, error, exclude ):
 	'''
-	Briskei to stoixeio me to megalutero frequency sth mia_lista
-	mono ean den to exei 3anabrei
+        Finds the element with the highest frequency in <mia_lista> that is not present in <exclude>. 
+        <mia_lista> is kmeans.labels_ list
+        <error> is a python list that for each different label in <mia_lista> will return the percentage of individuals that do not have the major label
+        <exclude> is a python list that contains already excluded labels
 	'''
 	count_dict=cl.Counter( mia_lista )
 	b=(max( [[count_dict[kleidi], kleidi] for kleidi in count_dict.keys() if kleidi not in exclude]))
@@ -257,10 +293,9 @@ def find_majority( mia_lista, error, exclude ):
 
 def k_means(np_table):
 	'''
-	Takes as input the output of my_pca function!
-	Kanei k-means analoga me to posoi diaforetikoi plh8usmoi uparxoun sto deigma
-	Kai briskei to error rate
-	'''
+        Performs kmeans analysis with clusters = the number of different populations and returns the percentage of individuals labeled incorrectly (<total_error>)
+	<np_table> is the output of "my_pca" function 
+        '''
 	header=pd.Series(np_table[:,0])
 	aa = read_my_labels(header)
 	genotypes_PCA=np_table[:,1:].astype(float)
@@ -273,45 +308,57 @@ def k_means(np_table):
 	total_error = sum(error)/kmeans.labels_.shape[0]
 	return total_error
  
-def find_ratio(data, sample_table, population_list, population_sizes ,independent=None, min_af = 0):
+def find_ratio(data, sample_table, population_list, population_sizes, independent = None, min_af = 0):
 	"""
-	Find the minimum # of dependent SNPs needed in the sample, so that the error is less than 0.1 
-	AND is stable (Neuton-Raphson criterion of stablility)
+	Find the minimum # of dependent SNPs needed in the sample, so that the error is less than 0.1. It outputs the # of SNPs and the eror_rate.
+        If the populations do not pass the error criterion after 1000 cycles, the function returns the current error rate as well.
+        <data> is the pandas DataFrame containing genotypes
+	<sample_table> is a pandas DataFrame containing information from the sample information file
+        <population_list> is a python list with all the names of the populations provided by the command line
+	<populaiton_sizes> is a python dictionary with keys = pop_names and values = how many individuals are to be simulated for each population
+        <independent> is the number of SNPs to be simulated. It is provided by command line
+	<min_af> is the lnowest acceptable frequency of a SNP
 	"""
 	simulation_table=simulation_independent(data,
-											sample_table,
-											population_list,
-											population_sizes,
-											args.independent,
-											min_af=args.MINIMUM_AF)
-	simulation_table=np.vstack((np.array(simulation_table.columns),simulation_table.as_matrix()))
-	pca_table=my_pca(simulation_table[1:,])
+						sample_table,
+						population_list,
+						population_sizes,
+						args.independent,
+                                                min_af=args.MINIMUM_AF)
+	simulation_table = np.vstack((np.array(simulation_table.columns),simulation_table.as_matrix()))
+	pca_table = my_pca(simulation_table[1:,])
 	k_means_value = k_means(np.column_stack((simulation_table[0,:],pca_table)))
 	counter = 0
-	while True :
+	while True:
 		# print(counter,
-				# simulation_table.shape[0]-independent-1,
-				# k_means_value
-				# )
+		# simulation_table.shape[0]-independent-1,
+		# k_means_value
+		# )
 		counter += 1
 		dependent_simulation_table=simulation_dependent(data, 
-														sample_table,
-														population_list,
-														population_sizes,
-														int( 20 ),
-														min_af=args.MINIMUM_AF).as_matrix()
-		simulation_table=np.vstack((simulation_table,dependent_simulation_table))
-		pca_table=my_pca(simulation_table[1:,])
+                                                                sample_table,
+								population_list,
+								population_sizes,
+								int( 20 ),
+								min_af=args.MINIMUM_AF).as_matrix()
+		simulation_table = np.vstack((simulation_table,dependent_simulation_table))
+		pca_table = my_pca(simulation_table[1:,])
 		error_rate = k_means(np.column_stack((simulation_table[0,:],pca_table)))
 		if error_rate<0.1 and abs(k_means_value - error_rate) < 0.01:
 			return [simulation_table.shape[0]-independent-1, error_rate ]
-		elif (counter >= 100):
+		elif (counter >= 1000):
 			return [simulation_table.shape[0]-independent-1, error_rate ]
 		else:
-			k_means_value=error_rate
+			k_means_value = error_rate
 
-def dendrogram (data, sample_table, independent=None, min_af = 0)  :
-		
+def dendrogram (data, sample_table, independent=None, min_af = 0):
+	'''
+        Constructs a dendrogram between all populations described in the <sample_table> using the error metrics produced by the "find_ratio" funtion
+        <data> is the pandas DataFrame containing genotypes
+	<sample_table> is a pandas DataFrame containing information from the sample information file
+        <independent> is the number of SNPs to be simulated. It is provided by command line
+        <min_af> is the lnowest acceptable frequency of a SNP
+        '''
 	population_sizes=dict(sample_table["pop"].value_counts())
 	populations=list(population_sizes.keys())
 	##populations= ["GBR","PUR","CLM","BEB","JPT","YRI"]
@@ -363,7 +410,7 @@ elif action == "PCA" : ### part 6
 	np_table=np.loadtxt(args.input_filename, delimiter="\t", dtype="object")
 	header=pd.Series(np_table[0,])
 	pca_table=my_pca(np_table[1:,])
-	pca_plot(pca_table, header)  ### for the plot 
+        pca_plot(pca_table, header, args.PCA_plot)  ### for the plot 
 	header=np.array(header)
 	np.savetxt(args.PCA_filename, np.column_stack((header,pca_table)) ,fmt="%s\t%f\t%f")
 elif action == "CLUSTER" : ##part 7
