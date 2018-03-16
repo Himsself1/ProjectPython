bfce20f9cb4fd9755b85722cca29321e790e5fef
marimall <35742496+marimall@users.noreply.github.com>
2018-03-16 11:32:27 +0200

    Update run_project3.bat

--- run_project3.bat
+++ run_project3.bat
@@ -1,18 +1,18 @@
 ::part 1
-py project3.py --vcf chr22_3000_lines.vcf --action VCF_INFO
+py project_python.py --vcf chr22_3000_lines.vcf --action VCF_INFO
 ::part 2
-py project3.py --vcf chr22_3000_lines.vcf --sample_filename sample_information.csv --action VALIDATE_SAMPLE_INFO
-py project3.py --sample_filename sample_information.csv --action SAMPLE_INFO
+py project_python.py --vcf chr22_3000_lines.vcf --sample_filename sample_information.csv --action VALIDATE_SAMPLE_INFO
+py project_python.py --sample_filename sample_information.csv --action SAMPLE_INFO
 ::part 3
-py project3.py --vcf chr22_3000_lines.vcf --sample_filename sample_information.csv --population GBR 100 --SNPs 100 --action SIMULATE --output outputfile.vcf
+py project_python.py --vcf chr22_3000_lines.vcf --sample_filename sample_information.csv --population GBR 100 --SNPs 100 --action SIMULATE --output outputfile.vcf
 ::part 4
-py project3.py --vcf chr22_3000_lines.vcf --sample_filename sample_information.csv --population GBR 100 --population FIN 100 --SNPs 100 --action SIMULATE --output outputfile.vcf
+py project_python.py --vcf chr22_3000_lines.vcf --sample_filename sample_information.csv --population YRI 100 --population FIN 100 --SNPs 100 --action SIMULATE --output outputfile.vcf
 ::part 5 
 
 ::py project3.py --vcf chr22_3000_lines.vcf --sample_filename sample_information.csv --population GBR 100 --SNPs 100 --output outputfile.vcf -- population FIN 100 -- population YRI 200 --independent 3000
-py project3.py --vcf chr22_3000_lines.vcf --sample_filename sample_information.csv --population FIN 100 --population YRI 200 --SNPs 500 --output outputfile.vcf --action SIMULATE --independent 300 --population GBR 300
+py project_python.py --vcf chr22_3000_lines.vcf --sample_filename sample_information.csv --population FIN 100 --population YRI 200 --SNPs 500 --output outputfile.vcf --action SIMULATE --independent 300
 ::part 6
-py project3.py --input_filename outputfile.vcf --PCA_filename pca.txt --PCA_plot pca.pdf --action PCA
+py project_python.py --input_filename outputfile.vcf --PCA_filename pca.txt --PCA_plot pca.pdf --action PCA
 ::part 7
 py project3.py --PCA_filename pca.txt  --action CLUSTER
 ::part 8
